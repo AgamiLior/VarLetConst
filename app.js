a9cc343938245b83e3b5345b7161c4da12c86cff
Lior Agami <agamilior25@gmail.com>
2021-03-21 13:10:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const PI = 3.14;
+PI = 42;
+console.log(PI)
+
+
+// ANSWERS TO QUESTIONS:
+
+// 1. "var" can be redeclared and reassigned and "let" can only be reassigned, also "var" is function scope where "let" isn't.
+// 2. "var" can be redeclared and reassigned and "const" cannot.
+// 3. "let" can only be reassigned, "const" cannot.
+// 4. Hoisting -
+//     the way variables read by the program, with "var" the variable "jump" to the top of the page and get declared as undefined, where "let" and "const" aren't hoisting.
